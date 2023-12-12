fbe50e8996824bc8e4f12015dfac60991eca0385
eXhumer <exhumer1@protonmail.com>
2023-12-12 12:51:54 -0700

    Add script files for Steam Deck

---
+++ hogwarts-legacy-steamdeck-fix.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if the new value is already present in GRUB_CMDLINE_LINUX_DEFAULT
+if grep -q "clearcpuid=514" /etc/default/grub; then
+  echo "The fix is already present in GRUB_CMDLINE_LINUX_DEFAULT"
+  sleep 3
+else
+  # Backup the original file
+  echo "Backup grub..."
+  sleep 1
+  cp /etc/default/grub /etc/default/grub.bak
+  
+  # Add the new value to GRUB_CMDLINE_LINUX_DEFAULT
+  echo "Adding parameter in grub..."
+  sleep 1
+  sed -i 's/^\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 clearcpuid=514"/' /etc/default/grub
+  
+  # Update the GRUB configuration file
+  echo "Update grub..."
+  sleep 1
+  update-grub
+  
+  # Reboot after a countdown
+  for i in {5..1}; do
+    echo "Reboot in $i seconds..."
+    sleep 1
+  done
+
+  echo "Reboot!"
+  shutdown now -r
+fi
---
+++ install-steamtinkerlaunch-git.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if script is not running as root
+if [[ $EUID > 0 ]]; then
+  echo "Please run as root"
+  exit
+else
+  # Disable SteamOS readonly mode
+  steamos-readonly disable
+
+  # Initialize and populate pacman keyring
+  pacman-key --init
+  pacman-key --populate archlinux holo
+
+  # Sync / refresh packages
+  pacman -Sy
+
+  # Install base-devel
+  pacman -S --noconfirm base-devel
+
+  # Remove old steamtinkerlaunch
+  rm -rf /home/deck/steamtinkerlaunch
+
+  # Clone latest steamtinkerlaunch from GitHub
+  git clone https://github.com/sonic2kk/steamtinkerlaunch.git /home/deck/steamtinkerlaunch
+
+  # Change directory to cloned steamtinkerlaunch
+  cd /home/deck/steamtinkerlaunch/
+
+  # Install steamtinkerlaunch
+  make install
+
+  # Add steamtinkerlaunch as Steam compatibility tool
+  sudo -u deck DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u deck)/bus steamtinkerlaunch compat add
+
+  # Enable SteamOS readonly mode
+  steamos-readonly enable
+fi
